d012f97453b34e6b13f2dd5d50dddce6b38f3fae
HFurkanBirel <hidayetfurkanbirel@gmail.com>
2023-01-08 00:43:40 +0300

    FPS Controller and teleportation added.

---
+++ Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class Gun : MonoBehaviour {
+
+    Transform cam;
+    [SerializeField] Movement movement;
+    [Header("LayerMasks")]
+    [SerializeField] private LayerMask teleportableLayer;
+    [SerializeField] private LayerMask pullableObjectLayer;
+
+    [Header("General Stats")]
+    [SerializeField] float range = 50f;
+    [SerializeField] float reloadTime;
+    WaitForSeconds reloadWait;
+    [SerializeField] float offsetDistance = 1.5f;
+
+    [Header("TeleportLaser")]
+    [SerializeField] GameObject laser;
+    [SerializeField] Transform muzzle;
+    [SerializeField] float fadeDuration = 0.3f;
+    [SerializeField] float destroyDuration = 0.8f;
+    [SerializeField] bool canTeleport;
+
+    [Header("Pulling")]
+    [SerializeField] bool canPull;
+
+    private void Awake ()
+    {
+        cam = Camera.main.transform;
+        reloadWait = new WaitForSeconds(reloadTime);
+    }
+
+    private void Start()
+    {
+        canTeleport = true;
+    }
+
+    public void ShootTeleport ()
+    {
+        RaycastHit hit;
+        Vector3 shootingDir = GetShootingDirection();
+        if (Physics.Raycast(cam.position, shootingDir, out hit, range, teleportableLayer))
+        {
+            Debug.Log("Hit Teleportable");
+            CreateLaser(hit.point); 
+            movement.Teleport(hit.point + hit.normal * offsetDistance);
+        }
+        else
+        {
+            Debug.Log("Didn't hit");
+            CreateLaser(cam.position + shootingDir * range);
+        }
+
+        canTeleport = false;
+    }
+
+    public void Pull()
+    {
+        RaycastHit hit;
+        Vector3 shootingDir = GetShootingDirection();
+        if (Physics.Raycast(cam.position, shootingDir, out hit, range, pullableObjectLayer))
+        {
+            Debug.Log("Hit Pullable");
+            CreateLaser(hit.point);
+
+        }
+        else
+        {
+            Debug.Log("Didn't hit");
+        }
+
+        canTeleport = false;
+    }
+
+    public IEnumerator FireTeleport()
+    {
+        if (canTeleport) {
+            ShootTeleport();
+            StartCoroutine(Reload());
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+
+    public IEnumerator PullObject()
+    {
+        if (canPull)
+        {
+
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        if (canTeleport) {
+            yield return null;
+        }
+        print("reloading...");
+        yield return reloadWait;
+        canTeleport = true; ;
+        print("finished reloading");
+        yield return null;
+    }
+
+    Vector3 GetShootingDirection()
+    {
+        Vector3 targetPos = cam.position + cam.forward * range;
+        Vector3 direction = targetPos - cam.position;
+
+        return direction.normalized;
+    }
+
+    void CreateLaser(Vector3 end)
+    {
+        GameObject laserObj = Instantiate(laser);
+        LineRenderer lr = laserObj.GetComponent<LineRenderer>();
+        lr.SetPositions(new Vector3[2] { muzzle.position, end });
+        StartCoroutine(FadeLaser(lr));
+        Destroy(laserObj, destroyDuration);
+    }
+
+    IEnumerator FadeLaser(LineRenderer lr)
+    {
+        float alpha = 1;
+        while (alpha > 0) {
+            alpha -= Time.deltaTime / fadeDuration;
+            lr.startColor = new Color(lr.startColor.r, lr.startColor.g, lr.startColor.b, alpha);
+            lr.endColor = new Color(lr.endColor.r, lr.endColor.g, lr.endColor.b, alpha);
+            yield return null;
+        }
+        Destroy(lr);
+    }
+}
---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+#pragma warning disable 649
+
+    [SerializeField] Movement movement;
+    [SerializeField] MouseLook mouseLook;
+    [SerializeField] Gun gun;
+
+    PlayerControls controls;
+    PlayerControls.PlayerInputActions playerInput;
+
+    Vector2 horizontalInput;
+    Vector2 mouseInput;
+
+    Coroutine fireCoroutine;
+    Coroutine pullCoroutine;
+
+    private void Awake ()
+    {
+        controls = new PlayerControls();
+        playerInput = controls.PlayerInput;
+
+        // groundMovement.[action].performed += context => do something
+        playerInput.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+        playerInput.Jump.performed += _ => movement.OnJumpPressed();
+
+        playerInput.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        playerInput.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
+
+        playerInput.Shoot.started += _ => StartFiring();
+        playerInput.Shoot.canceled += _ => StopFiring();
+
+        playerInput.Pull.started += _ => StartPulling();
+        playerInput.Pull.canceled += _ => StopPulling();
+    }
+
+    private void Update ()
+    {
+        movement.ReceiveInput(horizontalInput);
+        mouseLook.ReceiveInput(mouseInput);
+    }
+
+    void StartFiring()
+    {
+        fireCoroutine = StartCoroutine(gun.FireTeleport());
+    }
+
+    void StopFiring()
+    {
+        if (fireCoroutine != null) {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    void StartPulling()
+    {
+        gun.PullObject();
+    }
+
+    void StopPulling()
+    {
+        if (pullCoroutine != null)
+        {
+            StopCoroutine(pullCoroutine);
+        }
+    }
+
+    private void OnEnable ()
+    {
+        controls.Enable();
+    }
+
+    private void OnDestroy ()
+    {
+        controls.Disable();
+    }
+}
